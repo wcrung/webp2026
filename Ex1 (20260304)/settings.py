aef50114871acc40287defb247fa25c2bd8600ae
wcrung <crwang1006@gmail.com>
2026-03-04 23:06:42 +0800

    Create settings.py with app and logging configuration
    Add Django settings for installed apps and logging.

---
+++ Ex1 (20260304)/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+  'rest_framework',
+  'myapp',
+]
+
+LOGGING = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'handlers':{
+    'file':{
+      'level': 'DEBUG',
+      'class': 'logging.FileHandler',
+      'filename': 'debug.log',
+    },
+  },
+  'loggers':{
+    'django':{
+      'handlers': ['file'],
+      'level': 'DEBUG',
+      'propagate': True,
+    },
+  },
+}
