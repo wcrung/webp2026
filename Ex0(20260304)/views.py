dcc9c6a371d7ff1a967168735b7524dc3c24b899
wcrung <crwang1006@gmail.com>
2026-03-11 21:54:50 +0800

    Update views.py

--- Ex0(20260304)/views.py
+++ Ex0(20260304)/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    #my_name = request.GET.get('name', "CGU")
-    return HttpResponse("Hello Zoe"))
+    my_name = request.POST.get('name' , "CGU")
+    return HttpResponse("Hello " + my_name)
