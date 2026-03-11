057eba3a54787096682bd5e4c426cd3d21fb0d70
wcrung <crwang1006@gmail.com>
2026-03-11 21:40:49 +0800

    Update urls.py

--- Ex0(20260304)/urls.py
+++ Ex0(20260304)/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
-from .views import hello_api
+from . import views
+
 
 urlpatterns = [
-  path('api/hello/', hello_api),
+    path('', views.index, name='index'),
+]
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('myhello/', include('myhello.urls')),
 ]
