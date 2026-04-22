59bbbedb00c02049cd7e9fe5cccdec8be59699f3
wcrung <crwang1006@gmail.com>
2026-04-22 15:11:32 +0800

    Update script.js

--- Ex9/script.js
+++ Ex9/script.js
@@ -1,21 +1,28 @@
 function getImages(){
   const url="https://api.flickr.com/services/rest?method=flickr.photos.getRecent&api_key=ca370d51a054836007519a00ff4ce59e&per_page=9&format=json&nojsoncallback=1";
+  
   fetch(url)
-  .then(response=>response.json())
-  .then(data=>{
+  .then(response => response.blob())
+  .then(blob => blob.text())
+  .then(text => JSON.parse(text))
+  .then(data => {
     console.log(data);
-    const gallery=document.getElementById("gallery");
-    gallery.innerHTML=" ";
+
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
     
-    const photos=data.photos.photo;
-    photos.forEach(photo=>{
-      const img=document.vreateElemtn("img");
-      const imgUrl=           `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_w.jpg`;
-      img.src=imgUrl;
+    const photos = data.photos.photo;
+
+    photos.forEach(photo => {
+      const img = document.createElement("img");
+      
+      const imgUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_w.jpg`;
+      
+      img.src = imgUrl;
       gallery.appendChild(img);
     });
   })
-  .catch(error=>{
+  .catch(error => {
     console.log("Error:", error);
   });
 }
