0c6b924612b1ee3d7ba06f421b37f89032517a12
wcrung <crwang1006@gmail.com>
2026-04-29 15:36:31 +0800

    Update App.js

--- Ex10/src/App.js
+++ Ex10/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-const styleArgument={
-  fontSize: '100px',
-  color: 'red'
-};
-const changeText=(event)=>{
-  console.log(event.target);
-  event.target.innerText=event.target.innerText+"被點了";
-};
 
 function App(){
+  const [color, setColor]=useState('red');
+  const changeTextAndColor=(event)=>{
+    if(!event.target.innerText.includes("被點了")){
+      event.target.innerText=event.target.innerText+"被點了";
+    }
+    setColor('blue');
+  };
+  const styleArgument={
+    fontSize: '100px';
+    color: color
+  };
   return (
     <div className="App">
-      <h1 style={styleArgument} onClick={ChangeText}> Hello CGU!
+      <h1 style={styleArgument} onClick={changeTextAndColor}> Hello CGU!
       </h1>
     </div>
   );
 }
-
 export default App;
