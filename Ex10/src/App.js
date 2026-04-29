efd69787a8c86aeedbafba68de705e9c424a34d9
wcrung <crwang1006@gmail.com>
2026-04-29 15:13:15 +0800

    Create App.js

---
+++ Ex10/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+const styleArgument={
+  fontSize: '100px',
+  color: 'red'
+};
+const changeText=(event)=>{
+  console.log(event.target);
+  event.target.innerText=event.target.innerText+"被點了";
+};
+
+function App(){
+  return (
+    <div className="App">
+      <h1 style={styleArgument} onClick={ChangeText}>
+        Hello CGU!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
