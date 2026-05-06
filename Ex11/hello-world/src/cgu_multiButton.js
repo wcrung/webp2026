cd2c9f260a004b23aece982afde35dc630808507
wcrung <crwang1006@gmail.com>
2026-05-06 15:26:16 +0800

    Update cgu_multiButton.js

--- Ex11/hello-world/src/cgu_multiButton.js
+++ Ex11/hello-world/src/cgu_multiButton.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
+const MultiButton=()=>{
+  return (
+    <div>
+      <IconButton color="primary">
+        <AddShoppingCartIcon />
+      </IconButton>
+    
+      <IconButton color="secondary">
+        <DeleteIcon />
+      </IconButton>
+    
+      <IconButton color="default">
+        <AlarmIcon/>
+      </IconButton>
+    </div>
+  );
+};
+
+export default MultiButton;
