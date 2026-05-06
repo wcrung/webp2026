4ecd09abaaa71da0d03d79b0fc0f1f6d79e70d4a
wcrung <crwang1006@gmail.com>
2026-05-06 15:18:25 +0800

    Update App.js

--- Ex11/hello-world/src/App.js
+++ Ex11/hello-world/src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import MultiButton from './cgu_multiButton';
+import HelloCGU from './cgu_hello';
 
+function App(){
+  return(
+    <div style={{ textAlign: 'center'}}>
+      <div>
+        {HelloCGU()}
+      </div>
+         
+      <div style={{ marginTop: '30px'}}>
+        {MultiButton()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
