a7bd4a0d05fced41be654b234d57de0800817b60
wcrung <crwang1006@gmail.com>
2026-05-17 23:55:42 +0800

    Add CGU_Login component for user authentication

---
+++ HW5/src/cgu_login.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import { makeStyles } from '@mui/styles';
+
+function Copyright(props) {
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                CGU Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+
+    image: {
+        backgroundImage:
+            'url(https://obs.line-scdn.net/0h6s6X5vVbC0FvKj6vRz1fQm1pC2QWQx9TQkMIdXh3DSxKFl9aX0kLeW8rDXxXQF8f)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.mode === 'light'
+                ? theme.palette.grey[50]
+                : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+
+    paper: {
+        margin: '64px 32px', display: 'flex', flexDirection: 'column', alignItems: 'center',
+    },
+
+    avatar: {
+        margin: '8px', backgroundColor: '#9c27b0',
+    },
+
+    form: {
+        width: '100%', marginTop: '8px',
+    },
+
+    submit: {
+        marginTop: '24px',
+        marginBottom: '16px',
+    },
+}));
+
+function CGU_Login() {
+
+    const classes = useStyles();
+
+    return (
+        <Grid container component="main" className={classes.root}>
+
+            <CssBaseline />
+
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                className={classes.image}
+            />
+
+            <Grid
+                item
+                xs={12}
+                sm={8}
+                md={5}
+                component={Paper}
+                elevation={6}
+                square
+            >
+
+                <div className={classes.paper}>
+
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+
+                    <Typography component="h1" variant="h5">
+                        Sign in CSIE@CGU
+                    </Typography>
+
+                    <form className={classes.form} noValidate>
+
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                              
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    value="remember"
+                                    color="primary"
+                                />
+                            }
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                              
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    Don't have an account? Sign Up
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default CGU_Login;
